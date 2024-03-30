8cf8c89bd48ca94a93798c727a6940cc668b8bab
elijah <elireid.28@gmail.com>
2024-03-29 23:16:43 -0400

    added cors

--- DnDEZBackEnd/Program.cs
+++ DnDEZBackEnd/Program.cs
@@ -1,7 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            //replace localhost with yours
+            //also add your deployed website
+            policy.WithOrigins("http://localhost:4200",
+                                "https://MyChatRoom.com").AllowAnyMethod().AllowAnyHeader();
+        });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,5 +31,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors();
 
 app.Run();
